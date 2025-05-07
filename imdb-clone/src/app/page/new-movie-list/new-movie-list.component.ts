afa5b493631f37fb246798ce58a3fb34a5a08a67
Prithvi2505 <prithvigohil943@gmail.com>
2025-05-07 14:18:01 +0530

    search

--- imdb-clone/src/app/page/new-movie-list/new-movie-list.component.ts
+++ imdb-clone/src/app/page/new-movie-list/new-movie-list.component.ts
@@ -5,6 +5,8 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatDialog } from '@angular/material/dialog';
 import { UpdateMovieComponent } from '../update-movie/update-movie.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-new-movie-list',
@@ -12,19 +14,33 @@ import { UpdateMovieComponent } from '../update-movie/update-movie.component';
   styleUrl: './new-movie-list.component.css'
 })
 export class NewMovieListComponent {
+  movieList:any ;
+  dataSource:any;
+  @ViewChild(MatPaginator) paginator !:MatPaginator
+
+  displayedColumns: string[] = ['id', 'name', 'genre', 'releaseDate','duration', 'action','remove'];
+  filterColumns: string[] = ['f-id', 'f-name', 'f-genre', 'f-releaseDate', 'f-duration', 'f-action', 'f-remove'];
+
+
+  filterValues = {
+    id:'',
+    name: '',
+    genre:'',
+    releaseDate:'',
+    duration: ''
+  }
 
   constructor(private service : NewMovieService, private dialog:MatDialog, private router : Router) {
     this.loadMovies();
   }
 
-  movieList:any ;
-  dataSource:any;
-  @ViewChild(MatPaginator) paginator !:MatPaginator
+
 
   loadMovies(){
     this.service.getNewMovies().subscribe( res => {
       this.movieList = res;
       this.dataSource = new MatTableDataSource(this.movieList)
+      this.dataSource.filterPredicate = this.customFilterPrediction();
       this.dataSource.paginator = this.paginator
     })
   }
@@ -54,6 +70,24 @@ export class NewMovieListComponent {
     })
   }
   
-  displayedColumns: string[] = ['id', 'name', 'genre', 'releaseDate','duration', 'action','remove'];
+  filterChange(columnName: string, e: any) {
+    const value = e.target.value.trim().toLowerCase();
+    if (columnName in this.filterValues) {
+      this.filterValues[columnName as keyof typeof this.filterValues] = value;
+      this.dataSource.filter = JSON.stringify(this.filterValues);
+    }
+  }
+
+  customFilterPrediction(){
+    const filterPrediction = (data:any,filterValue:string):boolean => {
+      let searchstring = JSON.parse(filterValue);
+      return data.id.toString().trim().toLowerCase().indexOf(searchstring.id.toLowerCase()) !== -1 &&
+      data.name.toString().trim().toLowerCase().indexOf(searchstring.name.toLowerCase()) !== -1 &&
+      data.genre.toString().trim().toLowerCase().indexOf(searchstring.genre.toLowerCase()) !== -1 &&
+      data.releaseDate.toString().trim().toLowerCase().indexOf(searchstring.releaseDate.toLowerCase()) !== -1 &&
+      data.duration.toString().trim().toLowerCase().indexOf(searchstring.duration.toLowerCase()) !== -1 ;
+    }
+    return filterPrediction;
+  }
 
 }
